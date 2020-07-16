ee36b9033f3a39d0324a52f29802dc661c17badc
zyz19951225 <18258434805@163.com>
2020-07-16 14:28:00 +0800

    'commit'

---
+++ src/test/java/com/example/quartz_demo/QuartzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartz_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
